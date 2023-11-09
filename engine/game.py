086c2bd00585ca1516e74baf15cacbc970ee88b9
grx6741 <ikgowrish@gmail.com>
2023-11-09 22:34:29 +0530

    basic game

---
+++ engine/game.py
@@ -0,0 +1,83 @@
+from .map import Map
+from .player import Player
+from .light import LightSource
+from .camera import Camera
+
+import pygame
+from pygame import Vector2 as vec2
+
+class Game:
+    def __init__(self):
+        self.width = 1280
+        self.height = 720
+        self.__width = self.width//2
+        self.__height = self.height//2
+        self.game_is_running = True
+        self.FPS = 60
+
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.Surface((self.__width, self.__height))
+        self.fps = pygame.time.Clock()
+
+        self.maps = [
+            Map("assets/maps/default_map.csv", self.screen, self.screen.get_width() / 20),
+            Map("assets/maps/start_screen.csv", self.screen, self.screen.get_width() / 40)
+        ]
+        self.current_map = self.maps[1]
+
+        self.player = Player("assets/sprites/player.png", self.current_map.map_rect_width, self.screen)
+        self.light = LightSource(self.screen, self.player.pos, (80, 0, 0), 1000)
+
+        self.camera = Camera(self.screen, self.player, self.current_map)
+        self.offset = vec2(0, 0)
+        self.scroll = vec2
+        self.k = 1 / 20
+
+    def update(self):
+        if self.current_map.start_game_enabled:
+            self.current_map = self.maps[0]
+        pygame.display.set_caption(f"FPS: {int(self.fps.get_fps())}")
+        self.screen.fill((5, 5, 5))
+
+        # rects_in_vision = [rect for rect in self.current_map.map_rects if rect.colliderect(self.window.get_rect())]
+        self.light.pos = self.player.pos
+        # self.light.pos = vec2(self.screen.get_size())
+
+        self.offset += self.k * (self.player.pos - 0.5 * vec2(self.window.get_rect().center) - self.offset)
+        self.scroll = vec2(int(self.offset.x), int(self.offset.y))
+
+        # self.light.show(rects_in_vision, self.scroll)
+        self.light.show(self.current_map.nearby_rects, self.scroll)
+        self.camera.update(self.player.pos)
+
+        self.current_map.draw(self.scroll, self.player.pos)
+        self.player.move(self.current_map)
+        self.player.draw(self.scroll, self.current_map)
+
+        self.window.blit(pygame.transform.scale2x(self.screen), (0, 0))
+        pygame.display.update()
+        self.fps.tick(self.FPS)
+
+    def run(self):
+        while self.game_is_running and not self.current_map.end_game_enabled:
+            self.handle_key_events()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_is_running = False
+            self.update()
+        pygame.quit()
+        quit()
+
+    def handle_key_events(self):
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_a]:
+            self.player.vel += vec2(-self.player.speed, 0)
+        if key[pygame.K_d]:
+            self.player.vel += vec2(self.player.speed, 0)
+        if key[pygame.K_SPACE] and self.player.standing_on_ground:
+            self.player.jump()
+        if key[pygame.K_ESCAPE] and self.current_map == self.maps[0]:
+            self.game_is_running = False
+
+
---
+++ engine/player.py
@@ -0,0 +1,74 @@
+import pygame
+from .map import Map
+
+class Player:
+    def __init__(self, img_path: str, size: float, surface: pygame.Surface) -> None:
+        # self.img = pygame.image.load(img_path)
+        self.img = pygame.Surface((size, size))
+        self.img = pygame.transform.scale(self.img, (self.img.get_width() * size / self.img.get_height(), size))
+        self.size = size
+        self.img.fill((100, 100, 100))
+
+        self.gravity = 0.4
+        self.speed = 6
+        self.jump_force = 0.4 * size
+
+        self.pos = 0.5 * pygame.Vector2(surface.get_size()) - 0.5 * pygame.Vector2(self.img.get_size())
+        self.vel = pygame.Vector2(0, 0)
+        self.acc = pygame.Vector2(0, self.gravity)
+
+        self.surface = surface
+
+        self.rect = self.img.get_rect()
+        self.rect.centerx = int(self.pos.x)
+        self.rect.centery = int(self.pos.y)
+
+        self.standing_on_ground = False
+
+    def draw(self, cam_pos, map: Map):
+        if self.size != map.map_rect_width:
+            self.size = map.map_rect_width
+            self.img = pygame.transform.scale(self.img, (self.size, self.size))
+            self.rect = self.img.get_rect()
+            self.rect.centerx = int(self.pos.x)
+            self.rect.centery = int(self.pos.y)
+            self.jump_force = 0.4 * self.size
+        self.surface.blit(self.img, self.rect.move(-cam_pos))
+
+    def move(self, map: Map):
+        self.standing_on_ground = False
+        self.vel += self.acc
+
+        offsetted_rect = self.rect.copy()
+
+        offsetted_rect.x += int(self.vel.x)
+        collision_list = [tile for tile in map.map_rects if tile.colliderect(offsetted_rect)]
+        for tile in collision_list:
+            if self.vel.x < 0:
+                offsetted_rect.left = tile.right
+                self.vel.x *= 0
+            elif self.vel.x > 0:
+                offsetted_rect.right = tile.left
+                self.vel.x *= 0
+
+        offsetted_rect.y += int(self.vel.y)
+        collision_list = [tile for tile in map.map_rects if tile.colliderect(offsetted_rect)]
+        for tile in collision_list:
+            if self.vel.y < 0:
+                offsetted_rect.top = tile.bottom
+                if tile == map.start_game_rect:
+                    map.start_game_enabled = True
+                if tile == map.end_game_rect:
+                    map.end_game_enabled = True
+                self.vel.y *= 0
+            elif self.vel.y > 0:
+                offsetted_rect.bottom = tile.top
+                self.vel.y *= 0
+                self.standing_on_ground = True
+
+        self.rect = offsetted_rect
+        self.pos = pygame.Vector2(self.rect.center)
+        self.vel.x *= 0
+
+    def jump(self):
+        self.vel.y = -self.jump_force
